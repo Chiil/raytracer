a54e1bd32dc266dfe9265eafe4b930bfa8f40d69
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-03-07 23:05:24 +0100

    Added labels to axes

--- plot_results.py
+++ plot_results.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 
 itot = 256
 ktot = 128
+dx = 25
+
+x = np.arange(0, itot*dx, dx)
+z = np.arange(0, ktot*dx, dx)
 
 toa_up = np.fromfile('toa_up.bin', dtype=np.uint32)
 toa_down = np.fromfile('toa_down.bin', dtype=np.uint32)
@@ -14,13 +18,17 @@ print('out: ', surface_down.sum() + toa_up.sum() + atmos.sum())
 print('balance: ', toa_down.sum() - (surface_down.sum() + toa_up.sum() + atmos.sum()))
 
 plt.figure()
-plt.pcolormesh(atmos, cmap=plt.cm.plasma)
+plt.pcolormesh(x, z, atmos, shading='nearest', cmap=plt.cm.viridis)
+plt.xlabel('x (m)')
+plt.ylabel('z (m)')
 plt.colorbar()
 
 plt.figure()
-plt.plot(toa_down / toa_down.mean(), label='toa_down')
-plt.plot(toa_up / toa_down.mean(), label='toa_up')
-plt.plot(surface_down / toa_down.mean(), label='surface_down')
+plt.plot(x, toa_down / toa_down.mean(), label='toa_down')
+plt.plot(x, toa_up / toa_down.mean(), label='toa_up')
+plt.plot(x, surface_down / toa_down.mean(), label='surface_down')
 plt.legend(loc=0, frameon=False)
+plt.xlabel('x (m)')
+plt.ylabel('normalized irradiance (-)')
 
 plt.show()
