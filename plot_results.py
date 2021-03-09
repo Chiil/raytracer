8716ced39a9386e7aba465e9428cd0b223a7f662
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-03-09 14:13:18 +0100

    Add switch.

--- plot_results.py
+++ plot_results.py
@@ -9,6 +9,8 @@ ktot = 128
 dx = 25
 sigma_x = 40
 
+plot_raw_data = True
+
 x = np.arange(0.5*dx, itot*dx, dx)
 z = np.arange(0.5*dx, ktot*dx, dx)
 
@@ -89,18 +91,21 @@ plt.xlabel('x (m)')
 plt.ylabel('z (m)')
 
 plt.figure()
-plt.plot(x, surface_down, 'k:', linewidth=0.8, alpha=0.5)
+if plot_raw_data:
+    plt.plot(x, surface_down, 'k:', linewidth=0.8, alpha=0.5)
+    plt.plot(x, surface_down_direct, 'k:', linewidth=0.8, alpha=0.5)
+    plt.plot(x, surface_down_diffuse, 'k:', linewidth=0.8, alpha=0.5)
+    plt.plot(x, surface_up, 'k:', linewidth=0.8, alpha=0.5)
+    plt.plot(x, toa_down, 'k:', linewidth=0.8, alpha=0.5)
+    plt.plot(x, toa_up, 'k:', linewidth=0.8, alpha=0.5)
+
 plt.plot(x, surface_down_filtered, 'C0-', label='surf_dn')
-plt.plot(x, surface_down_direct, 'k:', linewidth=0.8, alpha=0.5)
 plt.plot(x, surface_down_direct_filtered, 'C0--', label='surf_dn_dir')
-plt.plot(x, surface_down_diffuse, 'k:', linewidth=0.8, alpha=0.5)
 plt.plot(x, surface_down_diffuse_filtered, 'C0:', label='surf_dn_dif')
-plt.plot(x, surface_up, 'k:', linewidth=0.8, alpha=0.5)
 plt.plot(x, surface_up_filtered, 'C1-', label='surf_up')
-plt.plot(x, toa_down, 'k:', linewidth=0.8, alpha=0.5)
 plt.plot(x, toa_down_filtered, 'C2-', label='toa_dn')
-plt.plot(x, toa_up, 'k:', linewidth=0.8, alpha=0.5)
 plt.plot(x, toa_up_filtered, 'C3-', label='toa_up')
+
 plt.legend(loc=0, ncol=3)
 plt.xlabel('x (m)')
 plt.ylabel('normalized irradiance (-)')
