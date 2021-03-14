b9b31726e23a0edb5ff68c0649856d6ce21feaa5
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-03-14 21:12:09 +0100

    Make the plotting j-index flexible.

--- python/plot_results_3d.py
+++ python/plot_results_3d.py
@@ -11,6 +11,7 @@ ktot = 128
 dx = 25
 sigma_x = 40
 
+j_plot = 120
 plot_raw_data = True
 
 x = np.arange(0.5*dx, itot*dx, dx)
@@ -50,7 +51,7 @@ atmos = atmos / norm
 
 # Plot the data.
 plt.figure()
-plt.pcolormesh(x, z, atmos[:, jtot//2, :], shading='nearest',
+plt.pcolormesh(x, z, atmos[:, j_plot, :], shading='nearest',
         cmap=plt.cm.viridis, vmin=atmos.min(), vmax=atmos.max())
 plt.colorbar()
 plt.xlabel('x (m)')
@@ -93,6 +94,7 @@ plt.xlabel('x (m)')
 plt.ylabel('y (m)')
 plt.title('surface_down_diffuse')
 
+"""
 plt.figure()
 plt.pcolormesh(x, y, surface_up, shading='nearest',
         cmap=plt.cm.viridis, vmin=surface_up.min(), vmax=surface_up.max())
@@ -118,16 +120,17 @@ plt.ylabel('y (m)')
 plt.title('toa_down')
 
 plt.figure()
-plt.plot(x, surface_down[jtot//2, :], 'C0-', label='surf_dn')
-plt.plot(x, surface_down_direct[jtot//2, :], 'C0--', label='surf_dn_dir')
-plt.plot(x, surface_down_diffuse[jtot//2, :], 'C0:', label='surf_dn_dif')
-plt.plot(x, surface_up[jtot//2, :], 'C1-', label='surf_up')
-plt.plot(x, toa_down[jtot//2, :], 'C2-', label='toa_dn')
-plt.plot(x, toa_up[jtot//2, :], 'C3-', label='toa_up')
+plt.plot(x, surface_down[j_plot, :], 'C0-', label='surf_dn')
+plt.plot(x, surface_down_direct[j_plot, :], 'C0--', label='surf_dn_dir')
+plt.plot(x, surface_down_diffuse[j_plot, :], 'C0:', label='surf_dn_dif')
+plt.plot(x, surface_up[j_plot, :], 'C1-', label='surf_up')
+plt.plot(x, toa_down[j_plot, :], 'C2-', label='toa_dn')
+plt.plot(x, toa_up[j_plot, :], 'C3-', label='toa_up')
 plt.legend(loc=0, ncol=3)
 plt.xlabel('x (m)')
 plt.ylabel('normalized irradiance (-)')
 plt.ylim(0, 1.3)
+"""
 
 
 # Filter the data
@@ -146,9 +149,8 @@ atmos_filtered = smooth_data_3d(atmos)
 
 
 # Plot the filtered data.
-"""
 plt.figure()
-plt.pcolormesh(x, z, atmos_filtered[:, jtot//2, :], shading='nearest',
+plt.pcolormesh(x, z, atmos_filtered[:, j_plot, :], shading='nearest',
         cmap=plt.cm.viridis, vmin=atmos.min(), vmax=atmos.max())
 plt.colorbar()
 plt.xlabel('x (m)')
@@ -168,33 +170,44 @@ plt.ylabel('y (m)')
 plt.title('surface_down_filtered')
 
 plt.figure()
-plt.pcolormesh(x, y, surface_up_filtered, shading='nearest',
-        cmap=plt.cm.viridis, vmin=surface_up.min(), vmax=surface_up.max())
+plt.pcolormesh(x, y, surface_down_direct_filtered, shading='nearest',
+        cmap=plt.cm.viridis, vmin=surface_down_direct.min(), vmax=surface_down_direct.max())
+plt.colorbar()
 rect = patches.Rectangle(
         (4000, 2700), 1000, 1000,
         edgecolor='k', facecolor='none', hatch='//')
 plt.gca().add_patch(rect)
-plt.colorbar()
 plt.xlabel('x (m)')
 plt.ylabel('y (m)')
-plt.title('surface_up_filtered')
-"""
+plt.title('surface_down_direct_filtered')
+
+plt.figure()
+plt.pcolormesh(x, y, surface_down_diffuse_filtered, shading='nearest',
+        cmap=plt.cm.viridis, vmin=surface_down_diffuse.min(), vmax=surface_down_diffuse.max())
+plt.colorbar()
+rect = patches.Rectangle(
+        (4000, 2700), 1000, 1000,
+        edgecolor='k', facecolor='none', hatch='//')
+plt.gca().add_patch(rect)
+plt.xlabel('x (m)')
+plt.ylabel('y (m)')
+plt.title('surface_down_diffuse_filtered')
 
 plt.figure()
 if plot_raw_data:
-    plt.plot(x, surface_down[jtot//2, :], 'k:', linewidth=0.8, alpha=0.5)
-    plt.plot(x, surface_down_direct[jtot//2, :], 'k:', linewidth=0.8, alpha=0.5)
-    plt.plot(x, surface_down_diffuse[jtot//2, :], 'k:', linewidth=0.8, alpha=0.5)
-    plt.plot(x, surface_up[jtot//2, :], 'k:', linewidth=0.8, alpha=0.5)
-    plt.plot(x, toa_down[jtot//2, :], 'k:', linewidth=0.8, alpha=0.5)
-    plt.plot(x, toa_up[jtot//2, :], 'k:', linewidth=0.8, alpha=0.5)
+    plt.plot(x, surface_down[j_plot, :], 'k:', linewidth=0.8, alpha=0.5)
+    plt.plot(x, surface_down_direct[j_plot, :], 'k:', linewidth=0.8, alpha=0.5)
+    plt.plot(x, surface_down_diffuse[j_plot, :], 'k:', linewidth=0.8, alpha=0.5)
+    plt.plot(x, surface_up[j_plot, :], 'k:', linewidth=0.8, alpha=0.5)
+    plt.plot(x, toa_down[j_plot, :], 'k:', linewidth=0.8, alpha=0.5)
+    plt.plot(x, toa_up[j_plot, :], 'k:', linewidth=0.8, alpha=0.5)
 
-plt.plot(x, surface_down_filtered[jtot//2, :], 'C0-', label='surf_dn')
-plt.plot(x, surface_down_direct_filtered[jtot//2, :], 'C0--', label='surf_dn_dir')
-plt.plot(x, surface_down_diffuse_filtered[jtot//2, :], 'C0:', label='surf_dn_dif')
-plt.plot(x, surface_up_filtered[jtot//2, :], 'C1-', label='surf_up')
-plt.plot(x, toa_down_filtered[jtot//2, :], 'C2-', label='toa_dn')
-plt.plot(x, toa_up_filtered[jtot//2, :], 'C3-', label='toa_up')
+plt.plot(x, surface_down_filtered[j_plot, :], 'C0-', label='surf_dn')
+plt.plot(x, surface_down_direct_filtered[j_plot, :], 'C0--', label='surf_dn_dir')
+plt.plot(x, surface_down_diffuse_filtered[j_plot, :], 'C0:', label='surf_dn_dif')
+plt.plot(x, surface_up_filtered[j_plot, :], 'C1-', label='surf_up')
+plt.plot(x, toa_down_filtered[j_plot, :], 'C2-', label='toa_dn')
+plt.plot(x, toa_up_filtered[j_plot, :], 'C3-', label='toa_up')
 
 plt.legend(loc=0, ncol=3)
 plt.xlabel('x (m)')
