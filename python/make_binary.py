349d123fee8202c2a02bca94279d38c61e64a5fe
MennoVeerman <menno@euw-vm-mv-slocs-o.vshf4n5etsvunkaa3ghannegbh.ax.internal.cloudapp.net>
2021-12-07 13:04:11 +0000

    fix make binary script and remove spurious cirrus layer

--- python/make_binary.py
+++ python/make_binary.py
@@ -4,10 +4,12 @@ import numpy as np
 ngpt = 19
 
 ncfile = nc.Dataset("cabauw_output_res.nc", "r")
-k_ext_cloud = ncfile.variables["kext_cloud"][1, :, :, :]
-k_ext_gas = ncfile.variables["kext_gas"][1, :, :, :]
-ssa = ncfile.variables["ssa"][1, :, :, :]
-asy = ncfile.variables["g"][1, :, :, :]
+k_ext_cloud = ncfile.variables["kext_cloud"][ngpt, :, :, :]
+k_ext_gas = ncfile.variables["kext_gas"][ngpt, :, :, :]
+ssa = ncfile.variables["ssa"][ngpt, :, :, :]
+asy = ncfile.variables["g"][ngpt, :, :, :]
+asy[265:269,:,:] = 0
+ssa[265:269,:,:] = ssa[264,:,:]
 
 k_ext_cloud.data.tofile("k_ext_cloud.bin")
 k_ext_gas.data.tofile("k_ext_gas.bin")
